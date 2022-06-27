cfd8951e0a9b789794eee1b83321cf3ee3e2c266
Gianou <david.gianadda@students.hevs.ch>
2022-06-27 20:54:58 +0200

    tutorial part 3 half of the way, Image reading bug fixed

--- src/main/java/Entity/Entity.java
+++ src/main/java/Entity/Entity.java
@@ -5,6 +5,6 @@ import java.awt.image.BufferedImage;
 public class Entity {
     public int x, y;
     public int speed;
-    public BufferedImage left2;
+    public BufferedImage still1, still2, left1,left2,right1,right2;
     public String direction;
 }
--- src/main/java/Entity/Player.java
+++ src/main/java/Entity/Player.java
@@ -6,6 +6,8 @@ import Main.KeyHandler;
 import javax.imageio.ImageIO;
 import java.awt.*;
 import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
 
 public class Player extends Entity{
@@ -20,14 +22,38 @@ public class Player extends Entity{
         getPlayerImage();
     }
     public void setDefaultValues(){
-        x = 100;
+        x = -100;
         y = 100;
-        speed = 12;
+        speed = 24;
         direction = "right";
     }
     public void getPlayerImage(){
         try {
-            left2 = ImageIO.read(getClass().getResourceAsStream("src/main/resources/BatLeft2.png"));
+
+            File file = new File("src/main/resources/BatStill1.png");
+            FileInputStream fis = new FileInputStream(file);
+            still1 = ImageIO.read(fis);
+
+            file = new File("src/main/resources/BatStill2.png");
+            fis = new FileInputStream(file);
+            still2 = ImageIO.read(fis);
+
+            file = new File("src/main/resources/BatLeft1.png");
+            fis = new FileInputStream(file);
+            left1 = ImageIO.read(fis);
+
+            file = new File("src/main/resources/BatLeft2.png");
+            fis = new FileInputStream(file);
+            left2 = ImageIO.read(fis);
+
+            file = new File("src/main/resources/BatRight1.png");
+            fis = new FileInputStream(file);
+            right1 = ImageIO.read(fis);
+
+            file = new File("src/main/resources/BatRight2.png");
+            fis = new FileInputStream(file);
+            right2 = ImageIO.read(fis);
+
         }
         catch (IOException e){
             e.printStackTrace();
@@ -50,10 +76,10 @@ public class Player extends Entity{
     }
     public void draw(Graphics2D g2){
 
-        BufferedImage image = null;
+        //BufferedImage image = null;
         //switch case
-        image = left2;
-        g2.drawImage(image, x, y, gp.tileSize, gp.tileSize, null);
+        BufferedImage image = left2;
+        g2.drawImage(image, x, y, gp.tileSize/2,gp.tileSize, null);
 
     }
 }
